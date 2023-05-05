f76c64816f584b1c2e2d3329a6499461032d265b
Mateus Andriola <mateusandriola18@gmail.com>
2023-05-05 19:37:36 -0300

    simple tailwind config

---
+++ src/defaultTodos.ts
@@ -0,0 +1,20 @@
+export const defaultTodoList: Todo[] = [
+  {
+    id: "1",
+    title: "Titulo 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam libero perspiciatis rerum aperiam consequuntur, blanditiis vel sapiente id dignissimos. Illum velit porro unde laborum reiciendis nesciunt, minima excepturi voluptatem ad.",
+  },
+  {
+    id: "2",
+    title: "Titulo 2",
+    description:
+      "Neste podcast: Onlyfans do Sr. K, pé frio, pets, macacos evil, anéis, teste do espelho… Mais uma vez, qual é a pauta?",
+  },
+  {
+    id: "3",
+    title: "Titulo 3",
+    description:
+      "Pedido de doação para Áurea Pires Romero que está no Hospital Bom Jesus, Congonhas/MG. Doações podem ser feitas em qualquer unidade do HEMOMINAS",
+  },
+];
